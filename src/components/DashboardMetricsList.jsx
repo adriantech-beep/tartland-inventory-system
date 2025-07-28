8bac3739f251dbe4d70fb8cc20b8b46624daa0e5
adriantech-beep <Adhrianne29@gmail.com>
2025-07-28 13:43:16 +0800

    fixed produced product component

--- src/components/DashboardMetricsList.jsx
+++ src/components/DashboardMetricsList.jsx
@@ -1,9 +1,17 @@
+import PureFlakesImg from "../assets/pure-flakes.png";
+import ChocoFlakesImg from "../assets/choco-flakes.png";
+
 const DashboardMetricsList = ({ produced }) => {
   const { flavor, totalBundles } = produced;
+  console.log(flavor);
 
+  const imagePath = flavor === "Pure Crunch" ? PureFlakesImg : ChocoFlakesImg;
   return (
     <div className="flex items-center justify-between p-5 rounded-2xl bg-white border border-cocoa/10 shadow hover:shadow-md transition-all">
       <div className="flex items-center justify-evenly w-full ">
+        <div className="max-w-24">
+          <img src={imagePath} />
+        </div>
         <p className="text-sm font-display text-cocoa dark:text-stone-800">
           {flavor}
         </p>
