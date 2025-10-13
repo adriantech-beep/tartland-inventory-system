c437cffff5b3c9819061d6c659ba8c5b8a1cf2cf
adriantech-beep <Adhrianne29@gmail.com>
2025-10-13 15:32:42 +0800

    Fixed dark mode

--- src/produce-product/StockSummarry.tsx
+++ src/produce-product/StockSummarry.tsx
@@ -1,17 +1,17 @@
 const StockSummary = ({ sorted }: any) => {
   return (
-    <div className="bg-white shadow-md rounded-xl p-4 border border-stone-200">
-      <h4 className="text-sm font-semibold text-stone-700 mb-3 flex items-center gap-1">
+    <div className="w-full mx-auto p-6 bg-card text-card-foreground rounded-xl border border-border shadow-sm space-y-6 transition-colors">
+      <h4 className="text-sm font-semibold text-stone-700 dark:text-stone-200 mb-3 flex items-center gap-1">
         <span>📊</span> <span>Current Stock Summary</span>
       </h4>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 text-xs text-stone-700">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 text-xs text-stone-700 dark:text-stone-300">
         {sorted.map((stock: any) => (
           <div
             key={stock._id}
-            className="flex justify-between items-center bg-stone-50 p-2 rounded-md border border-stone-200"
+            className="flex justify-between items-center  bg-card  p-2 rounded-md border border-stone-200 transition-colors duration-300"
           >
             <span className="truncate">{stock.name}</span>
-            <span className="font-semibold text-stone-800">
+            <span className="font-semibold text-stone-800 dark:text-stone-100">
               {stock.totalUnits.toFixed(2)} units
             </span>
           </div>
